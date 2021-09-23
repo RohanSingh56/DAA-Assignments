8002f21b3ebb13f2af9d5436dcf36e60dcbbccc1
Rohan <68508563+RohanSingh56@users.noreply.github.com>
2021-09-23 21:33:32 +0530

    Create Program2.cpp

---
+++ Week6/Program2.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool bipartiteBFS(int src,vector<int>adj[],int color[]){
+	queue<int> q;
+	q.push(src);
+	color[src]=1;
+	while(!q.empty()){
+		int node=q.front();
+		q.pop();
+		for(auto it : adj[node]){
+			if(color[it]==-1){
+				color[it]=1-color[node];
+				q.push(it);
+			}
+			else if(color[it]==color[node])
+				return false;
+		}
+	}
+	return true;
+}
+bool checkBipartite(int n,vector<int>adj[]){
+	int color[n];
+	memset(color,-1,sizeof color);
+	for(int i=0;i<n;i++){
+		if(color[i]==-1){
+			if(!bipartiteBFS(i,adj,color))
+				return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	int x;
+	int y;
+	cin >> x >> y;
+	vector<int>adj[x+1];
+	for(int i=0;i<y;i++)
+	{
+		int u,v;
+		cin>>u>>v;
+		adj[u].push_back(v);
+		adj[v].push_back(u);
+	}
+	if(checkBipartite(x,adj))
+		cout << "True" << endl;
+	else
+		cout << "False" << endl;
+	return 0;
+}

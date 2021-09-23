46577c480aeee9cc8d9fcef4699875024b942c05
Rohan <68508563+RohanSingh56@users.noreply.github.com>
2021-09-23 21:15:08 +0530

    Create Program3.cpp

---
+++ Week4/Program3.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+void swap(int &n1, int &n2)
+{
+    int t = n1;
+    n1 = n2;
+    n2 = t;
+}
+int partition(int arr[], int l, int r)
+{
+    int i = l-1;
+    int j= l;
+    while(j < r)
+    {
+        if(arr[j] <= arr[r])
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+        j++;
+    }
+    i++;
+    swap(arr[i], arr[r]);
+    return i;
+}
+int kSmallFind(int arr[], int l, int r, int k, int n)
+{
+    if(l <= r)
+    {
+        int p = partition(arr, l, r);
+        if(p == k-1)
+            return arr[p];
+        else if(p > k-1)
+           return kSmallFind(arr, l, p-1, k, n);
+        else
+            return kSmallFind(arr, p+1, r, k, n);
+    }
+    return -1;
+}
+int main()
+{
+    ifstream in;
+    in.open("input.txt");
+    ofstream out;
+    out.open("output.txt");
+    int t,size;
+    in>>t;
+    while(t--)
+    {
+        int k;
+        in>>size;
+        int arr[size];
+        for(int j=0;j<size;j++)
+            in>>arr[j];
+        in>>k;
+       int ans = kSmallFind(arr,0,(size-1),k,size);
+       if(ans==-1)
+        out << "Not Present\n";
+       else
+        out << ans << endl;
+    }
+    return 0;
+}
